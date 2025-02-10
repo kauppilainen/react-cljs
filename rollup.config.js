07f5c0897786c066109a7424d2d37e19645137ba
Felix Alm <felix.kauppi.alm@gmail.com>
2025-02-10 17:06:59 +0100

    Replace rollup-plugin-terser for @rollup/plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/index.js',
